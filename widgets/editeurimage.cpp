d984359d36f9dc0803c60777de95c962de2a5bd0
Sim-Pirs <theazx05@gmail.com>
2020-02-01 20:04:09 +0100

    ui editeur ready

--- widgets/editeurimage.cpp
+++ widgets/editeurimage.cpp
@@ -1,14 +1,55 @@
 #include "editeurimage.h"
 #include "ui_editeurimage.h"
 
+#include "clipscene.h"
+#include "QLabel"
+#include "QPixmap"
+#include "QImage"
+#include "QGraphicsView"
+#include "QtDebug"
+
 EditeurImage::EditeurImage(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::EditeurImage)
 {
     ui->setupUi(this);
+    m_graphicsView = ui->gView;
+    m_clippedLabel = new QLabel();
+
+    monImage = new QImage("/home/sim/Images/IMG_1919-3.jpg");
+
+    ui->gView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    ui->gView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    m_clipScene = new ClipScene(this, this->size());
+    m_graphicsView->setScene(m_clipScene);
+    m_clipScene->setImage("/home/sim/Images/IMG_1919-3.jpg");
+    m_clipScene->setRect();
+
+    connect(m_clipScene, &ClipScene::clippedImage, this, &EditeurImage::onClippedImage);
+
+    //connect(ui->valider, &QPushButton::pressed, this, &MainWindow::cropImageAndSave);
 }
 
 EditeurImage::~EditeurImage()
 {
     delete ui;
+}
+
+
+QSize EditeurImage::getSize(){
+    return this->size();
+}
+
+void EditeurImage::cropImageAndSave(){
+
+    QImage *image = new QImage();
+    *image = m_clippedLabel->pixmap()->toImage();
+    image->save("/home/sim/Images/wouatropbo.jpg");
+
+}
+
+void EditeurImage::onClippedImage(const QPixmap& pixmap)
+{
+    m_clippedLabel->setPixmap(pixmap);
 }
